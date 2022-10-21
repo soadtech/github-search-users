a1f50786c2d5a951d568fa4b714bcb78978ace63
Fernando Ropero <fropero@munily.com>
2022-10-21 14:57:43 -0500

    feat: adding debounce

--- src/modules/home/infrastructure/hooks/useHome.tsx
+++ src/modules/home/infrastructure/hooks/useHome.tsx
@@ -1,10 +1,23 @@
-import {ChangeEvent, useState} from "react";
+import {useCallback, ChangeEvent, useState} from "react";
+import {debounce} from "../../../../shared/utils";
 
 export default function useHome() {
     const [query, setQuery] = useState('')
     const [loading, setLoading] = useState(false)
+
+    function getUsersByQuery(value: string) {
+        console.log('value ', value)
+            setLoading(false)
+    }
+
+    const handleDebounce = useCallback(debounce(async function (textValue: string) {
+        getUsersByQuery(textValue)
+    }, 3000, false), [])
     function handlerChange(event: ChangeEvent<HTMLInputElement>) {
+        setLoading(true)
         const {value} = event.target
+        // @ts-ignore
+        handleDebounce(value)
         setQuery(value)
     }
     return {
--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -2,6 +2,7 @@
 export const debounce = (func: { apply: (arg0: any, arg1: IArguments) => void }, wait: number , immediate: boolean) => {
     let timeout: NodeJS.Timeout | null
     return function () {
+        // @ts-ignore
         const context = this
         const args = arguments
         const later = function () {
