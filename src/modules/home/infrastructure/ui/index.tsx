2a49e69b6d7cbc131e12fb777aedc487ac2ece43
Fernando Ropero <fropero@munily.com>
2022-10-21 15:57:38 -0500

    style: basic design card

--- src/modules/home/infrastructure/ui/index.tsx
+++ src/modules/home/infrastructure/ui/index.tsx
@@ -9,6 +9,12 @@ export default function Home() {
         <div className="container-home">
             <h1 className="text-white">Hi, i'm Home</h1>
             <SearchBox loading={loading} value={query} handlerChange={handlerChange} placeholder="Search a user" />
+            <div className="container-result">
+
+                    <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                    alt="image profile" style={{width: 50, height: 50, borderRadius: 150}}/>
+                    <span>Fernando Ropero</span>
+            </div>
         </div>
     )
 }
