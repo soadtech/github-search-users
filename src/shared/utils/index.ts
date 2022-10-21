2eb154bd98867d7c9cee3eccceeaed10486e6358
Fernando Ropero <fropero@munily.com>
2022-10-21 14:30:31 -0500

    feat: create func debounce

---
+++ src/shared/utils/index.ts
@@ -0,0 +1,20 @@
+
+export const debounce = (func: { apply: (arg0: any, arg1: IArguments) => void }, wait: number , immediate: boolean) => {
+    let timeout: NodeJS.Timeout | null
+    return function () {
+        const context = this
+        const args = arguments
+        const later = function () {
+            timeout = null
+            if (!immediate) {
+                func.apply(context, args)
+            }
+        }
+        const callNow = immediate && !timeout
+        clearTimeout(timeout as NodeJS.Timeout)
+        timeout = setTimeout(later, wait)
+        if (callNow) {
+            func.apply(context, args)
+        }
+    }
+}
